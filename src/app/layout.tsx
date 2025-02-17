1c5ab5450e9e7c584e3c5b29b2609eb43fe68c7e
mccartheney mendes <mccartheney@hotmail.com>
2025-02-17 15:41:35 +0000

    ADD| style on loginPage/redirect if logged

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={` antialiased`}
       >
