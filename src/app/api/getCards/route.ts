92982f993cdbcb52456281e6f34675703e9a7e38
mccartheney mendes <mccartheney@hotmail.com>
2025-02-22 21:53:39 +0000

    FEAT| getCards route impleamented

--- src/app/api/getCards/route.ts
+++ src/app/api/getCards/route.ts
@@ -1,3 +1,4 @@
+import getUser from "@/utils/getUser";
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -6,6 +7,26 @@ const prisma = new PrismaClient()
 const GET = async (
     req: NextRequest
 ) => {
+    // get email from params 
+    const { searchParams } = new URL(req.url);
+    const email = searchParams.get("email");
+
+    if (!email) return NextResponse.json({status:400, message:"Email is required"});
+
+    // get user from email
+    const user = await getUser(email)
+    if(!user) return NextResponse.json({status:404, message:"User not found"})
+
+    // try to get cards and return them
+    try {
+        const cards = await prisma.card.findMany({
+            where : {userId : user.id}
+        })
+
+        return NextResponse.json({status:200, message:cards})
+    } catch (error) {
+        return NextResponse.json({ status:500, message : "Problem getting cards" });
+    }
 
 }
 
