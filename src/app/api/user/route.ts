d5cbcc41358e4eb827bce1289e5b93445e8a800f
mccartheney mendes <mccartheney@hotmail.com>
2025-02-27 12:14:02 +0000

    FEAT: created a user api route to G and U users

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,82 @@
+import { PrismaClient } from "@prisma/client"
+import { NextRequest, NextResponse } from "next/server"
+
+const prisma = new PrismaClient()
+
+const GET = async (req: NextRequest) => {
+    // get user email
+    const {searchParams} = new URL (req.url)
+    const userEmail = searchParams.get("email")
+
+    // get users or user if the user send email
+    try {
+        if (userEmail) {
+            const user = await prisma.user.findFirst({
+                where : {email: userEmail}
+            })
+
+            if (!user) return NextResponse.json({status : 404, message : "User dont found"})
+
+            return NextResponse.json({ status: 200, message: user })    
+        }
+
+        const allUsers = await prisma.user.findMany ()
+        return NextResponse.json({status : 200, message:allUsers})
+    } catch (error) {
+        return NextResponse.json({ status: 500, message: "Error getting users" })
+    }
+
+}
+
+const PUT = async (req : NextRequest) => {
+    // get params from user
+    const {email, NewImageSrc, newUserName} = await req.json()
+
+    // get user
+    const user = await prisma.user.findFirst({
+        where : {email : email}
+    })
+    if (!user) return NextResponse.json({ status: 404, message: "User dont found" })
+
+    // if want to change image
+    if (NewImageSrc) {
+        try {
+            await prisma.user.update({
+                where: { email: email },
+                data: { image: NewImageSrc }
+            })
+
+            return NextResponse.json({ status: 200, message: "User profile picture updated" })
+        } catch (error) {
+            return NextResponse.json({ status: 500, message: "Error updating profile pic" })
+        }
+    }else if (newUserName) { // if want to change username
+        try {
+            await prisma.user.update({
+                where: { email: email },
+                data: { image: NewImageSrc }
+            })
+
+            return NextResponse.json({ status: 200, message: "User profile picture updated" })
+        } catch (error) {
+            return NextResponse.json({ status: 500, message: "Error updating profile pic" })
+        }
+    }
+
+    // remove profile pic
+    try {
+        const profilePicImagePh = "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+
+        await prisma.user.update ({
+            where : {email : email},
+            data: { image: profilePicImagePh }
+        })
+
+        return NextResponse.json({ status: 200, message: "User profile picture Removed" })
+    } catch (error) {
+        return NextResponse.json({ status: 500, message: "Error updating profile pic" })
+    }
+
+}
+
+export {GET, PUT}
